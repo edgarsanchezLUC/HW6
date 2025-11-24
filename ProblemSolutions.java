da25ba74c7d9c203550abae71731fbe0e33ec880
edgarsanchezLUC <151106977+edgarsanchezLUC@users.noreply.github.com>
2025-11-23 21:21:45 -0600

    Update ProblemSolutions.java
    Added name

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -2,7 +2,7 @@
 /******************************************************************
  *
  *   YOUR NAME / SECTION NUMBER
- *
+ *    Edgar Sanchez / 002
  *   This java file contains the problem solutions for the methods lastBoulder,
  *   showDuplicates, and pair methods. You should utilize the Java Collection
  *   Framework for these methods.
